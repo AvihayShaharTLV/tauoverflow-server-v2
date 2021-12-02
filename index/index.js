7e314cde78fb821e8575879da51c2904fd413f0b
Shon Salamon <shonsalamon@mail.tau.ac.il>
2021-12-02 18:02:42 +0200

    adding saving to s3 in server

--- index/index.js
+++ index/index.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const { postgraphile } = require("postgraphile");
+import { postgraphile} from 'postgraphile'
+import express from 'express'
+import cors from 'cors'
+import { generateUploadURL } from './s3.js'
 const app = express()
 const port = 5000
 
-app.options('*', cors()) 
+app.options('*', cors())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/s3Url', async (req, res) => {
+  console.log("trying to upload an image and generate URL")
+  const url = await generateUploadURL()
+  res.send({url})
 })
 
-app.use(
-    postgraphile(
-      process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/postgres",
-      "public",
-      {
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true,
-      }
-    )
-  );
+// app.use(
+//     postgraphile(
+//       process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/postgres",
+//       "public",
+//       {
+//         watchPg: true,
+//         graphiql: true,
+//         enhanceGraphiql: true,
+//       }
+//     )
+//   );
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+app.listen(port, () => console.log("listening on port " + port))
