40211aa61a8493a6b9a264613394523880d53fb9
Shon Salamon <shonsalamon@mail.tau.ac.il>
2022-01-04 00:28:08 +0200

    bug fixes with s3 keys and syntax conflicts bugs

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-import { postgraphile} from 'postgraphile'
-import express from 'express'
-import cors from 'cors'
-import { generateUploadURL } from './s3.js'
-const app = express()
-const port = 5000
+import { postgraphile } from "postgraphile";
+import express from "express";
+import cors from "cors";
+import { generateUploadURL } from "./s3.js";
+const app = express();
+const port = 5000;
 
-app.options('*', cors())
-app.use(cors())
+app.options("*", cors());
+app.use(cors());
 
-app.get('/s3Url', async (req, res) => {
-  const {folderName} = req.query;
-  console.log(folderName)
-  const url = await generateUploadURL(folderName)
-  res.send({url})
-})
+app.get("/s3Url", async (req, res) => {
+  const { folderName } = req.query;
+  console.log("uploading to s3 into folder:", folderName);
+  const url = await generateUploadURL(folderName);
+  res.send({ url });
+});
 
 app.use(
-    postgraphile(
-      process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/postgres",
-      "public",
-      {
-        watchPg: true,
-        graphiql: true,
-        enhanceGraphiql: true,
-      }
-    )
-  );
+  postgraphile(
+    process.env.DATABASE_URL ||
+      "postgres://postgres:password@localhost:5432/postgres",
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+  )
+);
 
-app.listen(port, () => console.log("listening on port " + port))
+app.listen(port, () => console.log("listening on port " + port));
